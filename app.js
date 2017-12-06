eabc13860f24ea52d0a315ed444b9e52610cfc2c
Jack N <jacknagle22@gmail.com>
2017-12-07 02:29:57 +1100

    added all the shits u need to make this work

---
+++ app.js
@@ -0,0 +1,27 @@
+const DiscordRPC = require("discord-rpc");
+const parse = require("parse-duration");
+const moment = require("moment");
+const openTimestamp = new Date();
+DiscordRPC.register("380058252123308035");
+
+const rpc = new DiscordRPC.Client({ transport: "ipc" });
+
+async function setActivity() {
+    if (!rpc) return;
+    const activity = {
+        details: "Best Multi-Purpose Bot!",
+        state: "https://pengubot.com",
+        largeImageKey: "pengu_logo",
+        largeImageText: "PenguBot",
+        instance: false,
+        startTimestamp: moment(openTimestamp).add(parse("-0s"), "ms").toDate()
+    };
+    rpc.setActivity(activity);
+}
+
+rpc.on("ready", () => {
+    console.log("Logged in with RPC!");
+    setActivity();
+});
+
+rpc.login("380058252123308035").catch(console.error);
